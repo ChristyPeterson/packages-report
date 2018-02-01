53ff618646fefe9b092f767971716d5cc04bea8c
Christy Peterson <christylynn.cross@gmail.com>
2018-02-01 13:39:16 -0600

    completed data generation step

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,9 @@
 ## deja vu from yesterday!
+library(tidyverse)
+library(here)
 
 ## create a data frame of your installed packages
+pack.df <- data.frame(installed.packages())
 
 ## keep the variables
 ##   * Package
@@ -8,11 +11,13 @@
 ##   * Version
 ##   * Priority
 ##   * Built
+pack.df <- pack.df %>% select(Package, LibPath, Version, Priority, Built)
 
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+write.csv(pack.df, file = here("data", "installed-packages.csv"))
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
